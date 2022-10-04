d48fdea692e5e3aa066b555e67c7a81bd0e9185f
Acacio de Lima <limadeacacio@gmail.com>
2022-10-04 04:09:58 -0300

    Update App.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,18 +1,14 @@
 import { Fragment } from "react";
 
-import useAuth from "./utils/hooks/useAuth";
-
 import Router from "./Router";
 import { Toastify } from "./components/Partials/Toastify";
 
 import "./styles/styles.css";
 
 function App() {
-  const { user } = useAuth();
-
   return (
     <Fragment>
-      <Router permissions={user?.permissions} />
+      <Router />
       <Toastify autoClose={5000} />
     </Fragment>
   );
